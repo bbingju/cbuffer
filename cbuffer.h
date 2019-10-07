c8991febb77937213e9b9ec5ecdae5e1bd71793e
Byung Ju Hwang <pjhwang@gmail.com>
2019-10-07 18:52:17 +0900

    Remove .c and add functions into .h instead

--- cbuffer.h
+++ cbuffer.h
@@ -51,10 +51,48 @@ __STATIC_INLINE int cbuffer_len(cbuffer_t *self)
         self->size - (self->tail - self->head);
 }
 
-int  cbuffer_push(cbuffer_t *self, uint8_t data);
-int  cbuffer_pop(cbuffer_t *self, uint8_t *data);
-int  cbuffer_peek(cbuffer_t *self, uint8_t *data, int index);
+__STATIC_INLINE void cbuffer_enqueue(cbuffer_t *self, uint8_t data)
+{
+    self->buffer[self->head] = data;
+    self->head = (self->head + 1) % self->size; /* next */
+}
+
+__STATIC_INLINE uint8_t cbuffer_dequeue(cbuffer_t *self)
+{
+    int tail = self->tail;
+    self->tail = (self->tail + 1) % self->size; /* next */
+    return self->buffer[tail];
+}
+
+__STATIC_INLINE int cbuffer_push(cbuffer_t *self, uint8_t data)
+{
+    if (cbuffer_isfull(self))
+        return -1;
+
+    cbuffer_enqueue(self, data);
+    return 0;
+}
+
+__STATIC_INLINE int cbuffer_pop(cbuffer_t *self, uint8_t *data)
+{
+    if (cbuffer_isempty(self))
+        return -1;
+
+    *data = cbuffer_dequeue(self);
+    return 0;
+}
+
+__STATIC_INLINE int cbuffer_peek(cbuffer_t *self, uint8_t *data, int index)
+{
+    int i = (self->tail + index) % self->size;
+
+    if (self->tail == self->head) /* is empty? */
+        return -1;
+
+    *data = self->buffer[i];
+    return 0;
+}
 
 #ifdef __cplusplus
 }
-#endif    
+#endif
